833b5fd07d86eccf759e2d18e0748102cf47819b
Richard Lin <richard.lin@berkeley.edu>
2014-11-24 22:32:29 -0800

    Add ValueDictView

---
+++ src/chisualizer/circuit/ValueDictView.py
@@ -0,0 +1,46 @@
+from Common import CircuitNode, HistoricalCircuitView
+
+class ValueDictView(HistoricalCircuitView):
+  """
+  View circuit state based on a value dict (from node paths to values
+  """
+  def __init__(self, width_dict):
+    self.value_dict = {}
+    self.width_dict = width_dict
+
+  def set_view(self, state):
+    self.value_dict = state
+  
+  def get_root_node(self):
+    return ValueDictNode(self, "")
+
+class ValueDictNode(CircuitNode):
+  def __init__(self, view, path):
+    self.view = view
+    self.path = path
+    
+  def get_type(self):
+    raise NotImplementedError("Node types not yet implemented")
+
+  def get_width(self):
+    assert self.path in self.view.width_dict
+    return self.view.width_dict[self.path]
+  
+  def get_depth(self):
+    raise NotImplementedError("ValueDict cannot yet save array-types")
+
+  def has_value(self):
+    return self.path in self.view.value_dict
+  
+  def can_set_value(self):
+    return False
+
+  def get_value(self):
+    return self.view.value_dict[self.path]
+  
+  def get_subscript_reference(self, subscript):
+    raise NotImplementedError("ValueDict cannot yet save array-types")
+  
+  def get_child_reference(self, child_path):
+    return ValueDictNode(self.view, self.join_path(self.path, child_path))
+  
